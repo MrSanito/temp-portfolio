69d19428e7a2eb760013c9cbff6fff74ab301cb4
unknown <vishalni2004@gmail.com>
2026-09-12 18:19:04 +0530

    ui alighment

--- app/resume/ResumeViewer.tsx
+++ app/resume/ResumeViewer.tsx
@@ -11,7 +11,7 @@ export default function ResumeViewer() {
 
   return (
     <div className="min-h-screen w-full bg-[#030303] text-foreground relative flex flex-col items-center justify-start overflow-x-hidden selection:bg-purple-500/30 selection:text-purple-200">
-      {/* Dynamic Background Pattern */}
+      {/* Dynamic Background Pattern */}  
       <div className="fixed inset-0 z-0 pointer-events-none bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
 
       {/* Main Resume Display */}
